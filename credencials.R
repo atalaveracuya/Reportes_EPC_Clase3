d7e5cb7c186a575680e04deaf06e95fcac821761
atalaveracuya <atalaveracuya@gmail.com>
2023-02-16 21:39:19 -0500

    Actualizado

---
+++ credencials.R
@@ -0,0 +1,6 @@
+###Configuracion de github #####
+usethis::use_git_config(user.name = "atalaveracuya",user.email="atalaveracuya@gmail.com")
+
+#usethis::create_github_token()
+
+credencials::set_github_pat("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")
